98168b7c0c1eb7665786ccd045ba8e1db4d511f3
Max <mingxuanlim@gmail.com>
2023-01-17 10:33:42 +0800

    refactor: fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ const config: Config.InitialOptions = {
 	moduleNameMapper: {
 		'(.+)\\.js': '$1',
 	},
-	extensionsToTreatAsEsm: ['.ts'],
-	transformIgnorePatterns: ['/node_modules/(?!(@zptxdev/zptx-lib))'],
+	extensionsToTreatAsEsm: ['.ts', '.js'],
+	transformIgnorePatterns: ['<rootDir>/node_modules/(?!(@zptxdev/zptx-lib))'],
 };
 export default config;
