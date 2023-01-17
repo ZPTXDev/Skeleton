b60b2cd4758d01b499b2136cb1701d111d668d83
Max <mingxuanlim@gmail.com>
2023-01-17 11:33:56 +0800

    refactor: use babel-jest again IDK MAN

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,13 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
 	roots: ['<rootDir>/src'],
 	transform: {
-		'^.+\\.[tj]sx?$': ['ts-jest', { useESM: true }],
+		'^.+\\.jsx?$': 'babel-jest',
+		'^.+\\.tsx?$': ['ts-jest', { useESM: true }],
 	},
 	moduleNameMapper: {
 		'(.+)\\.js': '$1',
 	},
 	extensionsToTreatAsEsm: ['.ts'],
 	transformIgnorePatterns: ['<rootDir>/node_modules/(?!(@zptxdev/zptx-lib))'],
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
 export default config;
