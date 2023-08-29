b0456a6eb5e8ef4c800d59c5477cefd2054d19cd
Max <max@zptx.dev>
2023-08-29 10:50:06 +0800

    docs(Logger): deprecate direct usage

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * from './Logger.js';
+import * as Logger from './Logger.js';
+/**
+ * @deprecated Use the logger provided by SkeletonClient instead.
+ */
+const DeprecatedLogger = Logger;
 export * from './SkeletonClient.js';
+export { DeprecatedLogger as Logger };
