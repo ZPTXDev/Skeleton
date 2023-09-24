42583261d07365f791ac2885e8ad84dc2fc22725
Max <max@zptx.dev>
2023-09-24 11:57:00 +0800

    fix: exports broken in latest release

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import * as Logger from './Logger.js';
+import * as DeprecatedLogger from './Logger.js';
+
 /**
  * @deprecated Use the logger provided by SkeletonClient instead.
  */
-const DeprecatedLogger = Logger;
+export const Logger = DeprecatedLogger;
+
 export * from './SkeletonClient.js';
-export { DeprecatedLogger as Logger };
