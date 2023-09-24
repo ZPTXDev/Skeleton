6724e03ab171e5bc092cad0630a133cc927b6fa8
Max <max@zptx.dev>
2023-09-24 12:01:22 +0800

    fix: consistent exports to maintain backwards compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import * as DeprecatedLogger from './Logger.js';
+import * as SkeletonClient from './SkeletonClient.js';
 
 /**
  * @deprecated Use the logger provided by SkeletonClient instead.
  */
-export const Logger = DeprecatedLogger;
+export const logger = DeprecatedLogger;
+
+/**
+ * @deprecated Use SkeletonClient instead.
+ */
+export const ZPTXClient = SkeletonClient;
 
 export * from './SkeletonClient.js';
